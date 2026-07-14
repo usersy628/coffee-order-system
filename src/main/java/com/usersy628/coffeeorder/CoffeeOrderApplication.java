836ea71bf8f59806247dd8e3321e44fc150e53cb
usersy628 <kimsy628@gmail.com>
2026-07-14 13:02:30 +0900

    chore: bootstrap Spring Boot project (#2)

---
+++ src/main/java/com/usersy628/coffeeorder/CoffeeOrderApplication.java
@@ -0,0 +1,12 @@
+package com.usersy628.coffeeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeOrderApplication.class, args);
+	}
+}
